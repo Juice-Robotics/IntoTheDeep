7cac1a29987f6a3c021a0b746c6f2863075d0e6d
awesomehtr1 <awesomehtr1@gmail.com>
2024-12-06 14:47:40 -0800

    cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/HPSideRedV1_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/HPSideRedV1_1.java
@@ -46,19 +46,15 @@ public class HPSideRedV1_1 extends LinearOpMode {
                 .setReversed(true)
                 .setTangent(Math.toRadians(-17))
                 .splineToLinearHeading(new Pose2d(38.5, -42.5, Math.toRadians(67)), Math.toRadians(67))
-                .waitSeconds(0.5)
                 .build();
         Action observation1 = drive.actionBuilder(new Pose2d(39.5, -43, Math.toRadians(67)))
                 .splineToLinearHeading(new Pose2d(35, -44.5, Math.toRadians(-55)), Math.toRadians(0))
-                .waitSeconds(0.5)
                 .build();
         Action spike2 = drive.actionBuilder(new Pose2d(35, -44.5, Math.toRadians(-55)))
                 .splineToLinearHeading(new Pose2d(51.5, -42, Math.toRadians(80)), Math.toRadians(70))
-                .waitSeconds(0.5)
                 .build();
         Action observation2 = drive.actionBuilder(new Pose2d(51.5, -42, Math.toRadians(80)))
                 .splineToLinearHeading(new Pose2d(38, -44.5, Math.toRadians(-60)), Math.toRadians(0))
-                .waitSeconds(0.5)
                 .build();
         Action back = drive.actionBuilder(new Pose2d(38, -44.5, Math.toRadians(-60)))
                 .setReversed(true)
@@ -67,11 +63,9 @@ public class HPSideRedV1_1 extends LinearOpMode {
                 .build();
         Action intake1 = drive.actionBuilder(new Pose2d(20, -35, Math.toRadians(-45)))
                 .setReversed(false)
-                .lineToX(26.5)
-//                .setTangent(9 * Math.PI/10)
-//                .splineToLinearHeading(new Pose2d(38, -44.5, Math.toRadians(-60)), 9 * Math.PI/10)
+                .lineToX(27)
                 .build();
-        Action back1 = drive.actionBuilder(new Pose2d(22, -37, Math.toRadians(-45)))
+        Action deposit = drive.actionBuilder(new Pose2d(22, -37, Math.toRadians(-45)))
                 .setTangent(9 * Math.PI/10)
                 .splineToLinearHeading(new Pose2d(-6, -27, Math.toRadians(-90)), Math.PI/2)
                 .build();
@@ -79,8 +73,6 @@ public class HPSideRedV1_1 extends LinearOpMode {
                 .setReversed(false)
                 .setTangent(-4*Math.PI/10)
                 .splineToLinearHeading(new Pose2d(23, -39,  Math.toRadians(-45)), 9*Math.PI/10)
-//                .setTangent(9 * Math.PI/10)
-//                .splineToLinearHeading(new Pose2d(38, -44.5, Math.toRadians(-60)), 9 * Math.PI/10)
                 .build();
         Action back2 = drive.actionBuilder(new Pose2d(23, -39, Math.toRadians(-45)))
                 .setTangent(9 * Math.PI/10)
@@ -91,15 +83,13 @@ public class HPSideRedV1_1 extends LinearOpMode {
                 .setReversed(false)
                 .setTangent(-4*Math.PI/10)
                 .splineToLinearHeading(new Pose2d(23, -39,  Math.toRadians(-45)), 9*Math.PI/10)
-//                .setTangent(9 * Math.PI/10)
-//                .splineToLinearHeading(new Pose2d(38, -44.5, Math.toRadians(-60)), 9 * Math.PI/10)
                 .build();
         Action back3 = drive.actionBuilder(new Pose2d(23, -39, Math.toRadians(-45)))
                 .setTangent(9 * Math.PI/10)
                 .splineToLinearHeading(new Pose2d(5, -27.5, Math.toRadians(-90)), Math.PI/2)
                 .waitSeconds(0.5)
                 .build();
-        telemetry.addData("is","starting");
+        telemetry.addData("Status","starting");
         telemetry.update();
         robot.initSubsystems();
         waitForStart();
@@ -111,11 +101,9 @@ public class HPSideRedV1_1 extends LinearOpMode {
                                 new ParallelAction(
                                         preloadDrive,
                                         new SequentialAction(
-//                                            new SleepAction(1),
                                             new InstantAction(robot::highRung)
                                         )
                                 ),
-//                                new SleepAction(0.5),
                                 robot.outtakeSpecimen(true),
                                 new InstantAction(() -> robot.lift.runToPosition(810)),
                                 new SleepAction(0.1),
@@ -127,17 +115,15 @@ public class HPSideRedV1_1 extends LinearOpMode {
 
                                 ),
                                 new InstantAction(()->robot.extension.runToPosition(225)),
-                                new SleepAction(1.4),
+                                new SleepAction(0.4),
                                 new InstantAction(() -> robot.arm.runToPreset(Levels.INTAKE_INTERMEDIATE)),
-//                                robot.commands.stopIntake(SampleColors.RED),
                                 observation1,
                                 robot.claw.ejectOpsAuton(true),
                                 new InstantAction(()->robot.extension.runToPosition(190)),
-                                new SleepAction(0.3),
                                 new InstantAction(() -> robot.arm.runToPreset(Levels.INTAKE)),
                                 spike2,
                                 new InstantAction(()->robot.extension.runToPosition(225)),
-                                new SleepAction(1.4),
+                                new SleepAction(0.4),
                                 new ParallelAction(
                                         new InstantAction(()->robot.extension.runToPosition(200)),
                                         observation2)
@@ -145,20 +131,16 @@ public class HPSideRedV1_1 extends LinearOpMode {
                                 robot.claw.ejectOpsAuton(true),
                                 back,
                                 //cycle1
-                                new SleepAction(0.9),
+                                new SleepAction(1),
                                 new ParallelAction(
                                     new SequentialAction(new SleepAction(0.5),
                                     new InstantAction(()->robot.extension.runToPosition(225))),
                                     intake1
                                 ),
-                                new SleepAction(1.4),
+                                new SleepAction(1),
                                 new ParallelAction(
-                                        back1,
+                                        deposit,
                                         new InstantAction(robot::highRung)
-//                                        new SequentialAction(
-//                                            new SleepAction(0.5),
-//
-//                                        )
                                 ),
                                 robot.outtakeSpecimen(true),
                                 new InstantAction(() -> robot.lift.runToPosition(810)),
@@ -169,7 +151,6 @@ public class HPSideRedV1_1 extends LinearOpMode {
                                 new SleepAction(0.5),
                                 new ParallelAction(
                                         intake2,
-
                                         new SequentialAction(
                                                 new SleepAction(0.1),
                                                 robot.retractedIntakePreset(true),
@@ -181,10 +162,6 @@ public class HPSideRedV1_1 extends LinearOpMode {
                                 new ParallelAction(
                                         back2,
                                         new InstantAction(robot::highRung)
-                //                                        new SequentialAction(
-                //                                            new SleepAction(0.5),
-                //
-                //                                        )
                                 ),
                                 robot.outtakeSpecimen(true),
                                 new InstantAction(() -> robot.lift.runToPosition(810)),
@@ -195,7 +172,6 @@ public class HPSideRedV1_1 extends LinearOpMode {
                                 new SleepAction(0.5),
                                 new ParallelAction(
                                         intake3,
-
                                         new SequentialAction(
                                                 new SleepAction(0.1),
                                                 robot.retractedIntakePreset(true),
@@ -207,98 +183,12 @@ public class HPSideRedV1_1 extends LinearOpMode {
                                 new ParallelAction(
                                         back3,
                                         new InstantAction(robot::highRung)
-                                        //                                        new SequentialAction(
-                                        //                                            new SleepAction(0.5),
-                                        //
-                                        //                                        )
                                 ),
                                 robot.outtakeSpecimen(true),
                                 new InstantAction(() -> robot.lift.runToPosition(810)),
                                 new SleepAction(0.1),
                                 new InstantAction(() -> robot.intermediatePreset())
 
-
-//
-//                                // SPIKE CENTER
-//                                new ParallelAction(
-//                                        driveToSpikeC,
-//                                        robot.intakePreset(50, true)
-//                                ),
-//                                robot.commands.stopIntake(SampleColors.RED),
-//                                new ParallelAction(
-//                                        driveToObC,
-//                                        new InstantAction(robot::preloadDropPreset)
-//                                ),
-//                                robot.outtakeSample(true),
-//
-//                                //SPIKE LEFT
-//                                new ParallelAction(
-//                                        driveToSpikeL,
-//                                        robot.intakePreset(50, true)
-//                                ),
-//                                robot.commands.stopIntake(SampleColors.RED),
-//                                new ParallelAction(
-//                                        driveToObL,
-//                                        new InstantAction(robot::preloadDropPreset)
-//                                ),
-//                                robot.outtakeSample(true),
-//
-//                                // **CYCLE #1**
-//                                driveToObservation1,
-//                                new InstantAction(() -> robot.intakePreset(50)),
-//                                robot.commands.stopIntake(SampleColors.RED),
-//                                new ParallelAction(
-//                                        driveToChamber1,
-//                                        new SequentialAction(
-//                                                new SleepAction(0),
-//                                                new InstantAction(robot::highRung)
-//                                        )
-//                                ),
-//                                robot.smartOuttake(true),
-//
-//                                // **CYCLE #2**
-//                                new ParallelAction(
-//                                        driveToObservation2,
-//                                        new SequentialAction(
-//                                                new SleepAction(0.1),
-//                                                robot.intakePreset(50, true)
-//                                        )
-//                                ),
-//                                robot.commands.stopIntake(SampleColors.RED),
-//                                new ParallelAction(
-//                                        driveToChamber2,
-//                                        new SequentialAction(
-//                                                new SleepAction(0),
-//                                                new InstantAction(robot::highRung)
-//                                        )
-//                                ),
-//                                robot.smartOuttake(true),
-//
-//                                // **CYCLE #3**
-//                                new ParallelAction(
-//                                        driveToObservation3,
-//                                        new SequentialAction(
-//                                                new SleepAction(0.1),
-//                                                robot.intakePreset(50, true)
-//                                        )
-//                                ),
-//                                robot.commands.stopIntake(SampleColors.RED),
-//                                new ParallelAction(
-//                                        driveToChamber3,
-//                                        new SequentialAction(
-//                                                new SleepAction(0),
-//                                                new InstantAction(robot::highRung)
-//                                        )
-//                                ),
-//                                robot.smartOuttake(true),
-//
-//                                // **PARK**
-//                                new ParallelAction(
-//                                        park,
-//                                        new SequentialAction(
-//                                                new InstantAction(robot::autonObParkPreset)
-//                                        )
-//                                )
                         ),
                         new LoopAction(() -> {
                             robot.lift.update();
