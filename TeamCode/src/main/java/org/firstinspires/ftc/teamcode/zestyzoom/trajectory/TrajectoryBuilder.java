7f0dd636293d2214ec44ed5e081e28e34496daf3
Siddharth Ray <sidraystudios@gmail.com>
2024-12-24 15:17:21 -0800

    turning yay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/zestyzoom/trajectory/TrajectoryBuilder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/zestyzoom/trajectory/TrajectoryBuilder.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.zestyzoom.trajectory;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.commands.primitives.SleepCommand;
 import org.firstinspires.ftc.teamcode.commands.util.Command;
@@ -22,6 +24,18 @@ public class TrajectoryBuilder {
     public TrajectoryBuilder addPoint(Pose2D target) {
         trajectory.add(new PointTrajectory(drive, target));
         endPose = target;
+        return this;
+    }
+
+    public TrajectoryBuilder turn(double angle) {
+        trajectory.add(new PointTrajectory(drive, new Pose2D(DistanceUnit.INCH, endPose.getX(DistanceUnit.INCH), endPose.getY(DistanceUnit.INCH), AngleUnit.RADIANS, endPose.getHeading(AngleUnit.RADIANS) + angle)));
+        endPose = new Pose2D(DistanceUnit.INCH, endPose.getX(DistanceUnit.INCH), endPose.getY(DistanceUnit.INCH), AngleUnit.RADIANS, endPose.getHeading(AngleUnit.RADIANS) + angle);
+        return this;
+    }
+
+    public TrajectoryBuilder turnTo(double heading) {
+        trajectory.add(new PointTrajectory(drive, new Pose2D(DistanceUnit.INCH, endPose.getX(DistanceUnit.INCH), endPose.getY(DistanceUnit.INCH), AngleUnit.RADIANS, heading)));
+        endPose = new Pose2D(DistanceUnit.INCH, endPose.getX(DistanceUnit.INCH), endPose.getY(DistanceUnit.INCH), AngleUnit.RADIANS, heading);
         return this;
     }
 
