46da294c69893f3b4b35ba987beb891e766936c4
Siddharth Ray <sidraystudios@gmail.com>
2024-12-23 18:06:13 -0800

    commands yay

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/util/Commands.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.commands.util;
+
+public class Commands {
+    public void runBlocking(Command command) {
+        boolean running = true;
+        while (running && !Thread.currentThread().isInterrupted()) {
+            command.run();
+        }
+    }
+}
